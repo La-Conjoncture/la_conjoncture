44c50cdd8ca9c78f7fbe0c2b21bed124002746c4
Emmanuelle Nadal <manue.nadal@gmail.com>
2023-02-24 18:28:08 +0100

    Feat: added main layout and home's content

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ module.exports = {
   ],
   theme: {
     theme: {
-      extend: {},
+      colors: {
+        'cream': '#FCF6F6',
+        'black': '#050F08',
+        'grey': '#D5D8D6',
+        'blue': '#0060D6'
+      },
+      extend: {
+        fontFamily: {
+          syne: ['Syne Mono', 'ui-sans-serif']
+        },
+      },
     },
   },
   plugins: []
