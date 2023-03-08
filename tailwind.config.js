c53a1e645966ed155b409bbed040051a029d85d1
Emmanuelle Nadal <manue.nadal@gmail.com>
2023-03-08 17:26:40 +0100

    Feat: added navbar (simple version)

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,17 @@ module.exports = {
     './*.html',
   ],
   theme: {
-    theme: {
-      colors: {
-        'cream': '#FCF6F6',
-        'black': '#050F08',
-        'grey': '#D5D8D6',
-        'blue': '#0060D6'
-      },
-      extend: {
-        fontFamily: {
-          syne: ['Syne Mono', 'ui-sans-serif']
-        },
+    colors: {
+      'cream': '#FCF6F6',
+      'black': '#050F08',
+      'grey': '#D5D8D6',
+      'blue': '#0060D6',
+      'ocre': '#852E25'
+    },
+    extend: {
+      fontFamily: {
+        display: ['Syne Mono', 'ui-sans-serif'],
+        body: ['Satoshi', 'ui-sans-serif'],
       },
     },
   },
