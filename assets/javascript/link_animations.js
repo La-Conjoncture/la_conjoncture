5cb865ddeea8e5735c96e90a0bbca3fb0229c027
Emmanuelle Nadal <manue.nadal@gmail.com>
2023-05-23 15:14:23 +0200

    Feat: fninshed projects layout

--- assets/javascript/link_animations.js
+++ assets/javascript/link_animations.js
@@ -3,6 +3,9 @@ const bracketLinks = document.querySelectorAll('.bracket-link');
 bracketLinks.forEach(link => {
   link.addEventListener('mouseenter', event => {
     const annotation = RoughNotation.annotate(event.target, { type: 'bracket', color: '#852E25', animationDuration: 500, strokeWidth: 3, brackets: ['left', 'right']});
+    if (link.classList.contains('white-bracket-link')) {
+      annotation.color = 'white';
+    }
     annotation.show();
 
     link.addEventListener('mouseleave', event => {
